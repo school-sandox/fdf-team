71a799a5507e99fd85cc7c616d058a45d7b7d0f9
Jace Macgyver <jmacgyve@at-d1.21-school.ru>
2019-08-17 15:56:32 +0300

    version-01

---
+++ version-01/fdf.h
@@ -0,0 +1,34 @@
+#ifndef FDF_H
+#define FDF_H
+
+#define MAX_INT 2147483647
+#define MIN_INT -2147483648
+
+#define ERR_USAGE   "Usage: ./fdf <name-file.fdf>"
+#define ERR_MALLOC_MAP_INITIALIZE "Malloc map_initialize Error."
+#define ERR_MAP_INITIALIZE "Invalid Map. Check your map."
+#define ERR_READ_MAP "Reading fail"
+
+
+typedef struct s_point{
+	int x;
+	int y;
+	int z;
+} t_point;
+
+typedef struct s_map{
+	void* mlx_ptr;
+	void* win_ptr;
+    int width;
+    int height;
+    int size;
+    int z_max;
+    int z_min;
+	t_point *points;
+} t_map;
+
+t_map			*map_initialize(const char *file);
+void            print_error(char *message);
+int             read_map(const char *file, t_map *map);
+
+#endif
